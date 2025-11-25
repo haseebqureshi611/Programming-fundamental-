c3f0b115ffe7527c2c65c346fd2f9e4cf7dd6436
Haseeb Qureshi <has3eb112@gmail.com>
2025-11-25 11:39:47 +0500

    Add files via upload

---
+++ haseeb.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "-----------------------------\n";
+    cout << "\tStudent ID Card\n";
+    cout << "-----------------------------\n\n";
+
+    cout << "Name:\t\tHaseeb Qureshi\n";
+    cout << "Roll No:\tSet-25-42\n";
+    cout << "Department:\tsoftware engineering technology\n";
+    cout << "Session:\t2025-2029\n";
+
+    return 0;
+}
